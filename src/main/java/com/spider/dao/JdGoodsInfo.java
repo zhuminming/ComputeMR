12b907833e71ab009eddfb59877142558cbf8822
zmm <minming.zhu@51job.com>
2017-02-24 19:08:15 +0800

    add

--- src/main/java/com/spider/dao/JdGoodsInfo.java
+++ src/main/java/com/spider/dao/JdGoodsInfo.java
@@ -7,5 +7,41 @@ public class JdGoodsInfo {
 	private int    good_buy;        //商品购买数量
 	private String good_provider;   //商品供应商
 	private String good_type;       //商品类别
+	public int getGood_id() {
+		return good_id;
+	}
+	public void setGood_id(int good_id) {
+		this.good_id = good_id;
+	}
+	public String getGood_name() {
+		return good_name;
+	}
+	public void setGood_name(String good_name) {
+		this.good_name = good_name;
+	}
+	public float getGood_price() {
+		return good_price;
+	}
+	public void setGood_price(float good_price) {
+		this.good_price = good_price;
+	}
+	public int getGood_buy() {
+		return good_buy;
+	}
+	public void setGood_buy(int good_buy) {
+		this.good_buy = good_buy;
+	}
+	public String getGood_provider() {
+		return good_provider;
+	}
+	public void setGood_provider(String good_provider) {
+		this.good_provider = good_provider;
+	}
+	public String getGood_type() {
+		return good_type;
+	}
+	public void setGood_type(String good_type) {
+		this.good_type = good_type;
+	}
 
 }
--- src/main/java/com/spider/parse/HtmlParseInterface.java
+++ src/main/java/com/spider/parse/HtmlParseInterface.java
@@ -1,5 +1,10 @@
 package com.spider.parse;
 
+import java.util.List;
+
+import com.spider.dao.JdGoodsInfo;
+
 public interface HtmlParseInterface {
+	public List<JdGoodsInfo> ParseHtml (String html);
 
 }
